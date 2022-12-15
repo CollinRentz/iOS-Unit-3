0d4c4b06ba063d677c55ac13008ac34699b4bea6
Collin Rentz <rentzcollin@gmail.com>
2022-12-15 14:32:39 -0700

    “_”

---
+++ day 3/mediaManager/MediaManager/Controllers/View Controllers/HomeTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  HomeTableViewController.swift
+//  MediaManager
+//
+//  Created by Collin Rentz 11/23/22.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(reminderFired), name: Notification.Name(rawValue: "mediaReminderNotification"), object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+    }
+    
+    // MARK: - Methods
+    @objc func reminderFired() {
+        DispatchQueue.main.async {
+            self.tableView.backgroundColor = .red
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.tableView.backgroundColor = .systemBackground
+            self.view.backgroundColor = .systemBackground
+        }
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "homeCell", for: indexPath)
+
+        if indexPath.row == 0 {
+            cell.textLabel?.text = "Favorites"
+        } else if indexPath.row == 1 {
+            cell.textLabel?.text = "Movies"
+        } else {
+            cell.textLabel?.text = "TV Shows"
+        }
+        return cell
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toMediaItemVC" {
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let destination = segue.destination as! MediaItemTableViewController
+            destination.items = MediaItemController.shared.sections[indexPath.row]
+        }
+    }
+}
---
+++ day 3/mediaManager/MediaManager/Controllers/View Controllers/MediaItemTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MediaItemTableViewController.swift
+//  MediaManager
+//
+//  Created by Collin Rentz 11/23/22.
+//
+
+import UIKit
+
+class MediaItemTableViewController: UITableViewController {
+    
+    var items: [MediaItem] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mediaItemCell", for: indexPath)
+
+        let item = items[indexPath.row]
+
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = String(item.rating)
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         if segue.identifier == "toMediaItemDetailVC" {
+             guard let indexPath = tableView.indexPathForSelectedRow else { return }
+             let destination = segue.destination as! MediaItemDetailViewController
+             destination.delegate = self
+             destination.mediaItem = items[indexPath.row]
+         }
+     }
+}
+
+extension MediaItemTableViewController: DeleteItemDelegate {
+    func deleteItem(mediaItem: MediaItem) {
+        guard let index = items.firstIndex(of: mediaItem) else { return }
+        items.remove(at: index)
+        tableView.reloadData()
+    }
+}
